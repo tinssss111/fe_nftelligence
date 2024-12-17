d77470dd8b6ec0a5993b8da0ff9d127944a5a684
ntins <vonhuttin123456789@gmail.com>
2024-12-17 17:24:35 +0700

    deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -11,5 +13,5 @@ export default {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')], 
+  plugins: [typography],
 };
